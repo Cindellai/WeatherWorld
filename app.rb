61c34240ed8845b2a4ca507e26196af0ca941076
Chinemelum Iloegbunam <52719448+Cindellai@users.noreply.github.com>
2024-05-28 05:53:29 +0000

    commit 1

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'curb'
+require 'json'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get '/' do
+  api_key = 'b0c17454395e1db0360dd8382332e2f4'
+  city = params[:city] || 'London' # Default to 'London' if no city is provided
+  req = Curl::Easy.perform("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{api_key}")
+  res = req.body_str
+  @data = JSON.parse(res)
+  erb :index
 end
